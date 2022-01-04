e43194a647b1ce136f9bf705f89e079361ee6b00
Ryan1997123 <47643573+Ryan1997123@users.noreply.github.com>
2022-01-04 00:22:43 -0500

    Race Day Program

--- raceDay.js
+++ raceDay.js
@@ -5,9 +5,11 @@ Race Day Program
 
 /*Task:
 
-CodeCademy’s annual race is just around the corner! This year, we have a lot of participants. You have been hired to write a program that will register runners for the race and give them instructions on race day.
+CodeCademy’s annual race is just around the corner! This year, we have a lot of participants. 
+I have been hired to write a program that will register runners for the race and give them instructions on race day.
 
-Here’s how our registration works. There are adult runners (over 18 years of age) and youth runners (under 18 years of age). They can register early or late. Runners are assigned a race number and start time based on their age and registration.
+Here’s how our registration works. There are adult runners (over 18 years of age) and youth runners (under 18 years of age). 
+They can register early or late. Runners are assigned a race number and start time based on their age and registration.
 
 Race number:
 
